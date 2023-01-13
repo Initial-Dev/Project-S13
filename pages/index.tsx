cf38b0ddcfb938f493f5ad5bb9abbda8baed8bf1
Louis Bellefemine <bellefemine.louis@gmail.com>
2023-01-13 13:52:47 +0100

    trying to fix pm2 refresh

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js !!!
+            Next.js :)
           </a>
         </h1>
 
