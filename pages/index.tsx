1b169aaf93e9f15b25638c57283d8ca19d5acd3b
Louis Bellefemine <bellefemine.louis@gmail.com>
2023-01-13 13:43:08 +0100

    changing title to Kameground

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Kameground</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
