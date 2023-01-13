460be2bb59117c4b12648b5e4efb1e61690a10a2
Louis Bellefemine <bellefemine.louis@gmail.com>
2023-01-13 15:18:57 +0100

    testing the new CI-CD

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          By Louis - Get started by editing{' '}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.tsx
           </code>
