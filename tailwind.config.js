8ce7d339b7bb62ad99baa2f14957edcf986eb249
Hugo PEREIRA PIO <hugo.pereirapio@viacesi.fr>
2023-02-21 21:50:07 +0100

    Add plugins vidstack, forms, filters to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('vidstack/tailwind.cjs'),
+		require('@tailwindcss/forms'),
+		require('tailwindcss-filters'),
+	],
+};
