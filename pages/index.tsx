1c229595a622c6024d78e2e4f61a08de40e782e7
Hugo Pereira Pio <hugo.pereira-pio@thinkproject.com>
2023-03-02 13:42:31 +0100

    update css

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
 					<ul
 						role="list"
-						className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-4 "
+						className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 "
 					>
 						{videosList.map(
 							({
