f23bebbc3b2a924629bcc7da92e06a5561bff6a9
Hugo PEREIRA PIO <hugo.pereirapio@viacesi.fr>
2023-03-04 19:22:02 +0100

    add props

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,17 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import mp4ToHls from "./toolbox/mp4ToHls";
-import {Exception} from "@jest/types/build/Circus";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import mp4ToHls from './toolbox/mp4ToHls';
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
+	req: NextApiRequest,
+	res: NextApiResponse<any>
 ) {
-  try {
-    mp4ToHls('test.mp4')
-    res.status(200).json({etat: 'ok'});
-  } catch (e) {
-    res.status(200).json({ erreur: e });
-  }
-
+	try {
+		mp4ToHls('test.mp4');
+		res.status(200).json({ etat: 'ok' });
+	} catch (e) {
+		res.status(200).json({ erreur: e });
+	}
 }
