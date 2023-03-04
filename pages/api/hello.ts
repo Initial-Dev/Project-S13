f9538dbfb7556ed202fea8bee2e436ad2bf7973d
Louis Bellefemine <bellefemine.louis@gmail.com>
2023-03-04 18:21:03 +0100

    fix wrong import

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import mp4ToHls from "./toolbox/mp4ToHls";
-import {Exception} from "@jest/types/build/Circus";
 
 export default function handler(
   req: NextApiRequest,
