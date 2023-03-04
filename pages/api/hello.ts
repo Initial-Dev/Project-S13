fa78a101325cf7a58effd8d3d071d0ca8fb97177
YannisGJ <93491318+YannisGJ@users.noreply.github.com>
2023-03-05 00:47:50 +0100

    Upload to S3 Bucket work but still in work in progress

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import upload from "./toolbox/upload";
+import upload from "./upload";
 import mp4ToHls from "./toolbox/mp4ToHls";
 
 export default function handler(
--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,15 +1,30 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from 'express'
 import nc from 'next-connect'
+import aws from 'aws-sdk'
 import multer, { FileFilterCallback } from 'multer'
-import mp4ToHls from "./toolbox/mp4ToHls";
+import mp4ToHls from "./toolbox/mp4ToHls"
 
 export const config = {
     api: {
       bodyParser: false
     }
   }
-  
+
+  const sizeLimit = 300; // 300 Mo
+
+  // Configuration de AWS
+  const s3 = new aws.S3({
+    accessKeyId: 'AKIAR4WLWUN5N3PLOQ5Z',
+    secretAccessKey: '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',
+    region: 'eu-west-3',
+  });
+
+  const params = {
+    Bucket: 'kamegroundbucket',
+    Key: 'test.mp4', // <=== Need to be changed
+  };
+
 // Configuration de Multer
 const upload = multer({
   storage: multer.diskStorage({
@@ -24,7 +39,7 @@ const upload = multer({
     }
     callback(null, true)
   },
-  limits: { fileSize: 300 * 1024 * 1024 }, // 300 Mo
+  limits: { fileSize: sizeLimit * 1024 * 1024 },
 })
 
 const apiRoute = nc<NextApiRequest, NextApiResponse>()
@@ -42,10 +57,20 @@ apiRoute.post((req: NextApiRequest & Request, res: NextApiResponse & Response) =
     }
     // Le fichier est disponible dans req.file
     res.status(200).json({ message: "Upload Done!", file: req.file })
-    
     if(filename)
       mp4ToHls(filename.toString())
+
+    // Upload to Amazon S3
+    // Need to put a await here
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        console.log('Error:', err);
+      } else {
+        console.log('Success:', data);
+      }
+    });
   })
+  
 })
 
 export default apiRoute
