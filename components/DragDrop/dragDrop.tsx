6ee316701c79d5bf7f3fb5f40c6b3afaf300e55d
Louis Bellefemine <bellefemine.louis@gmail.com>
2023-03-05 18:26:54 +0100

    layout done

--- components/DragDrop/dragDrop.tsx
+++ components/DragDrop/dragDrop.tsx
@@ -8,15 +8,17 @@ type Props = {
 }
 const dragDrop: FunctionComponent<Props> = ({className, icon, description}) => {
     return (
-        <div className={`flex flex-col items-center gap-3 h-36 w-1/2 rounded-lg border-dashed border-[1px] border-white ${className}`}>
-            <input hidden/>
-            <div className={"mt-5 h-1/4 w-1/4"}>
-                { icon }
-            </div>
-            <div className={"text-xs"}>
-                { description }
-            </div>
-        </div>
+        <>
+            <input type={"file"} id={"upload"} className={"hidden"}/>
+            <label htmlFor={"upload"} className={`flex flex-col items-center gap-3 h-36 w-1/2 rounded-lg border-dashed border-[1px] border-white ${className}`}>
+                <div className={"mt-5 h-1/4 w-1/4"}>
+                    { icon }
+                </div>
+                <div className={"text-xs text-center"}>
+                    { description }
+                </div>
+            </label>
+        </>
     )
 }
 
--- pages/upload/uploadDesktop.tsx
+++ pages/upload/uploadDesktop.tsx
@@ -6,6 +6,7 @@ import DragDrop from "../../components/DragDrop";
 import OptionsUpload from "../../components/OptionsUpload";
 import {AiOutlineCloudUpload} from "react-icons/ai";
 import {BiImage} from "react-icons/bi";
+import CallToAction from "../../components/CallToAction";
 
 type Props = {
     data: dataToSend
@@ -15,19 +16,19 @@ const uploadDesktop: FunctionComponent<Props> = (data) => {
         <div className={"w-full h-screen absolute top-0 flex flex-col items-center justify-center"}>
             <div className={"flex flex-col items-center h-3/5 w-3/4 bg-[#77777733] rounded-3xl"}>
                 <Title className={""}/>
-                <div className={"h-full w-5/6 flex flex-col mt-3"}>
+                <div className={"h-full w-5/6 flex flex-col justify-center"}>
                     <FormUpload className={""}/>
                     <div className={"mt-5 flex flex-row"}>
                         <div className={"w-1/2 flex flex-row p-5 gap-5 justify-center"}>
                             <DragDrop icon={<AiOutlineCloudUpload className={"w-full h-full"}/>} description={"MP4 ou WebM"} />
                             <DragDrop icon={<BiImage className={"h-full w-full"} />} description={"Couverture / Miniature"}/>
                         </div>
-                        <div>
+                        <div className={"w-1/2"}>
                             <OptionsUpload />
                         </div>
                     </div>
-                    <div className={"mt-5"}>
-                        hop
+                    <div className={"mt-5 flex flex-row justify-end w-full"}>
+                        <CallToAction />
                     </div>
                 </div>
             </div>
