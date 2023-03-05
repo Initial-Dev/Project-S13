472df74bdb99a674c7cd51c6b720db2b978dfdbd
YannisGJ <93491318+YannisGJ@users.noreply.github.com>
2023-03-05 19:39:28 +0100

    Upload : the folder will be sent after the conversion and the tmp file and folder will be deleted after sending

--- pages/api/toolbox/mp4ToHls.ts
+++ pages/api/toolbox/mp4ToHls.ts
@@ -1,6 +1,7 @@
 import * as process from "process";
 import fs from 'fs-extra';
 import sendToS3 from './sendToS3';
+import rimraf from 'rimraf';
 
 export default async function mp4ToHls (filename: String) {
     const ffmpeg = require('fluent-ffmpeg');
@@ -9,10 +10,12 @@ export default async function mp4ToHls (filename: String) {
     ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
     console.log(process.cwd())
-    
+
+    const filesname = filename.split('.')[0];
     const foldername = filename.split('.')[0];
     fs.ensureDirSync(`${process.cwd()}/pages/api/hls/${foldername}`);
 
+    let state = false;
     ffmpeg(`${process.cwd()}/pages/api/tmp/${filename}`, {
         timeout: 432000
     }).addOptions([
@@ -22,10 +25,11 @@ export default async function mp4ToHls (filename: String) {
         '-hls_time 6',
         '-hls_list_size 0',
         '-f hls'
-    ]).output(`${process.cwd()}/pages/api/hls/${foldername}/test.m3u8`).on('end', () => {
+    ]).output(`${process.cwd()}/pages/api/hls/${foldername}/${filesname}.m3u8`).on('end', () => {
         console.log('conversion fini !')
-    }).run();
-
-    await sendToS3(`${process.cwd()}/pages/api/hls/${foldername}`, 'kamegroundbucket', foldername);
+        sendToS3(`${process.cwd()}/pages/api/hls/${foldername}`, 'kamegroundbucket', foldername).finally(() => {
+            rimraf(`${process.cwd()}/pages/api/hls/${foldername}`);
+            rimraf(`${process.cwd()}/pages/api/tmp/${filename}`);
+    })}).run();
 
 }
