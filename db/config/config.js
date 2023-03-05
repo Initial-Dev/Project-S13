3cd35b31703ddc180c6c2c9fc6290ffda6729538
eleve <gmatii740@gmail.com>
2023-03-05 17:08:20 +0100

    database connected

--- db/config/config.js
+++ db/config/config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   development: {
-    username: "kameground",
-    password: "postgres",
-    database: "kameground",
-    host: "postgres",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     dialectOptions: {
       ssl: false,
